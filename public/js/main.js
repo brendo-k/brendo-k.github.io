b5bc6c4ef50c623d06f52ed21aeda8816861cd4a
brendo-k <brenden.kadota@gmail.com>
2020-11-03 14:54:07 -0800

    added navbar and integrated hexo into blog page

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,26 @@
-var path = window.location.pathname;
-var base = path.replace(/\.[^/.]+$/, "").split('/');
-var active = '.' + base[base.length-1];
-$(document).ready(function(){
-  $('.navbar').load("/html/navbar.html", function(){
-    if(active !== '.'){
-      $(active).addClass('active');
-    }
-  });
-});
+const navbar_toggle = document.querySelector(".navbar-button");
+const navbar_icon = document.querySelector(".navbar-toggler-icon");
+const navbar = document.querySelector('.navbar');
+const nav_items = document.querySelector('.navbar-nav');
+
+let showMenu = false;
+
+navbar_toggle.addEventListener('click', toggle_menu);
+
+
+
+
+function toggle_menu(){
+  if(!showMenu){
+    navbar_icon.classList.add('open');
+    navbar.classList.add('open');
+    nav_items.classList.add('open');
+    showMenu = true;
+  } else{
+    navbar_icon.classList.remove('open');
+    navbar.classList.remove('open');
+    nav_items.classList.remove('open');
+    showMenu = false;
+  }
+}
+
