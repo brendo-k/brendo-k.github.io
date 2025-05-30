ae6b28312772913f728b0febe902b4910396b6d8
Brenden <brenden.kadota@gmail.com>
2025-05-30 08:00:31 -0700

    uploaded docs folder for public access

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: '/brendo-k.github.io/',  // Replace <REPO_NAME> with the actual name of your GitHub repo
   build: {
-    outDir: '..'
+    outDir: 'docs'
   }
 })
